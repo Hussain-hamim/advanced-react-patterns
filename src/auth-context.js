5c3e53869ce3e35244c12889c11b81aec59644fe
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-21 09:37:26 +0430

    Context Module Functions

--- src/auth-context.js
+++ src/auth-context.js
@@ -7,6 +7,7 @@ const AuthContext = React.createContext({
   user: {username: 'jakiechan', tagline: '', bio: ''},
 })
 AuthContext.displayName = 'AuthContext'
+
 const AuthProvider = ({user, ...props}) => (
   <AuthContext.Provider value={user} {...props} />
 )
