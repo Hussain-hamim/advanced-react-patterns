8dd495a302e032f3135bb6b3c49a4810b244e522
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-21 22:14:51 +0430

    Flexible Compound Components with context(final)

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,46 +1,37 @@
-// Flexible Compound Components
-// http://localhost:3000/isolated/exercise/03.js
+// Flexible Compound Components with context
+// http://localhost:3000/isolated/final/03.js
 
 import * as React from 'react'
 import {Switch} from '../switch'
 
-// 🐨 create your ToggleContext context here
 const ToggleContext = React.createContext()
-// 📜 https://react.dev/reference/react/createContext
+ToggleContext.displayName = 'ToggleContext'
 
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  const value = {on, toggle}
 
-  // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
-  // the value is an object that has `on` and `toggle` on it.
   return (
-    <ToggleContext.Provider value={value}>{children}</ToggleContext.Provider>
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
   )
 }
 
-// 🐨 we'll still get the children from props (as it's passed to us by the
-// developers using our component), but we'll get `on` implicitly from
-// ToggleContext now
-// 🦉 You can create a helper method to retrieve the context here. Thanks to that,
-// your context won't be exposed to the user
-
-const useToggle = () => React.useContext(ToggleContext)
-// 📜 https://react.dev/reference/react/useContext
+function useToggle() {
+  return React.useContext(ToggleContext)
+}
 
 function ToggleOn({children}) {
   const {on} = useToggle()
   return on ? children : null
 }
 
-// 🐨 do the same thing to this that you did to the ToggleOn component
 function ToggleOff({children}) {
   const {on} = useToggle()
   return on ? null : children
 }
 
-// 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
 function ToggleButton({...props}) {
   const {on, toggle} = useToggle()
   return <Switch on={on} onClick={toggle} {...props} />
@@ -61,8 +52,3 @@ function App() {
 }
 
 export default App
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
