2d83d717545a6eba293741403850d9bd9813a1d0
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-24 13:13:18 +0430

    Control Props: add a controlled state warning - standard solution

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -29,18 +29,23 @@ function toggleReducer(state, {type, initialState}) {
     }
   }
 }
-function useControlledSwitchWarning(onIsControlled) {
-  const {current: onWasControlled} = React.useRef(onIsControlled)
+function useControlledSwitchWarning(
+  controlPropValue,
+  controlPropName,
+  componentName,
+) {
+  const isControlled = controlPropValue != null
+  const {current: wasControlled} = React.useRef(isControlled)
   React.useEffect(() => {
     warning(
-      !(onIsControlled && !onWasControlled),
-      `\`useToggle\` is changing from uncontrolled to be controlled. Components should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled \`useToggle\` for the lifetime of the component. Check the \`on\` prop.`,
+      !(isControlled && !wasControlled),
+      `\`${componentName}\` is changing from uncontrolled to be controlled. Components should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled \`useToggle\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`,
     )
     warning(
-      !(!onIsControlled && onWasControlled),
-      `\`useToggle\` is changing from controlled to be uncontrolled. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled \`useToggle\` for the lifetime of the component. Check the \`on\` prop.`,
+      !(!isControlled && wasControlled),
+      `\`${componentName}\` is changing from controlled to be uncontrolled. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled \`useToggle\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`,
     )
-  }, [onIsControlled, onWasControlled])
+  }, [componentName, controlPropName, isControlled, wasControlled])
 }
 
 function useToggle({
@@ -56,7 +61,7 @@ function useToggle({
   const onIsControlled = controlledOn != null
   const on = onIsControlled ? controlledOn : state.on
 
-  useControlledSwitchWarning(onIsControlled)
+  useControlledSwitchWarning(controlledOn, 'on', 'useToggle')
 
   const hasOnChange = Boolean(onChange)
   React.useEffect(() => {
